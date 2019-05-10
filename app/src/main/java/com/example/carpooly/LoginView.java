8f6810cbfb9c684ff99d9bdde884a29d721035d9
Samuel Sachnoff <ssachnof@calpoly.edu>
2019-05-09 20:00:18 -0700

    tmp

---
+++ app/src/main/java/com/example/carpooly/LoginView.java
@@ -0,0 +1,24 @@
+package com.example.carpooly;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LoginView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String username = intent.getStringExtra(LoginController.USERNAME_MESSAGE);
+
+        //display the username
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setText(username);
+
+        //display the password
+    }
+}
