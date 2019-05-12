8ff3ce16cf108e1442ffae92677ac294e8966f58
Samuel Sachnoff <ssachnof@calpoly.edu>
2019-05-12 13:24:52 -0700

    Rough Registration GUI Completed

---
+++ app/src/main/java/com/example/carpooly/RegistrationView.java
@@ -0,0 +1,10 @@
+package com.example.carpooly;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RegistrationView extends AppCompatActivity{
+
+}
