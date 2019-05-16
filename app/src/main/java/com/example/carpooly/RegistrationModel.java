31d1e4d92219251b9971cff58611d86add07e84b
Samuel Sachnoff <ssachnof@calpoly.edu>
2019-05-16 10:19:13 -0700

    finished login page

--- app/src/main/java/com/example/carpooly/RegistrationModel.java
+++ app/src/main/java/com/example/carpooly/RegistrationModel.java
@@ -27,8 +27,12 @@ import java.io.OutputStream;
 import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.KeyStore;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
+
+import javax.xml.parsers.FactoryConfigurationError;
+
 public class RegistrationModel {
     public boolean createUser(String username, String pass, Context context) throws IOException {
         System.out.println("username: " + username);
@@ -44,33 +48,27 @@ public class RegistrationModel {
                 lines += (char) fs.read();
             }
             JsonObject jo = (JsonObject) parser.parse(lines);
-            System.out.print(lines);
-            JsonObject tmp = new JsonObject();
-            tmp.add("password", new JsonPrimitive("blah"));
-            JsonObject tmp1 = new JsonObject();
-            tmp1.addProperty("password", "blah");
             jo.getAsJsonObject("users").add(username, parser.parse("{password: " + pass + "}"));
-            System.out.println(jo.toString());
             String filename = "users.json";
             FileOutputStream outputStream;
             outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
             outputStream.write(jo.toString().getBytes());
             outputStream.close();
             return true;
-//            FileInputStream fis = context.openFileInput("temp.json");
-//            InputStreamReader isr = new InputStreamReader(fis);
-//            BufferedReader bufferedReader = new BufferedReader(isr);
-//            StringBuilder sb = new StringBuilder();
-//            String line;
-//            while ((line = bufferedReader.readLine()) != null) {
-//                sb.append(line);
-//            }
-//            System.out.println("line: "+ sb.toString());
-//
-//            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+        } catch (FileNotFoundException e) {
+            try {
+                FileOutputStream outputStream;
+                JsonObject jo = new JsonObject();
+                jo.add("users", parser.parse("{" + username + ":" + " {password: " + pass + "}}"));
+                System.out.println(jo.toString());
+                outputStream = context.openFileOutput("users.json", Context.MODE_PRIVATE);
+                outputStream.write(jo.toString().getBytes());
+                outputStream.close();
+                return true;
+            } catch (Exception e1) {
+                e1.printStackTrace();
+                return false;
+            }
         }
     }
 }
