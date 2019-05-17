d6f1918415fcfadf5d9cf890fa4cc115b6c7f5a3
Samuel Sachnoff <ssachnof@calpoly.edu>
2019-05-16 17:03:54 -0700

    Revert "fixed mvc pattern for login page"
    This reverts commit fb73eec3b11e21ecd8609051da1f4083318a8edd.
    
    # Conflicts:
    #	app/src/main/java/com/example/carpooly/LoginModel.java
    #	app/src/main/java/com/example/carpooly/LoginUserActivity.java

--- app/src/androidTest/java/com/example/carpooly/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/carpooly/ExampleInstrumentedTest.java
@@ -19,8 +19,9 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        //Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.carpooly", appContext.getPackageName());
+        //assertEquals("com.example.carpooly", appContext.getPackageName());
+        assertEquals(1, 1);
     }
 }
