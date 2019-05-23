6e2b293a00eace8942b6b581076da509a2e6f1c4
DHalland <dylanhalland@gmail.com>
2019-05-22 18:38:34 -0700

    method signatures (template)

--- app/src/main/java/com/example/carpooly/Post.java
+++ app/src/main/java/com/example/carpooly/Post.java
@@ -1,14 +1,19 @@
 package com.example.carpooly;
 
-public class Offer {
-    // public void createOfferPost
-        // this will include information such as driver name and rating,
-        // departure time and date, destination, max # of riders, route
-    // public void removeOfferPost
+public abstract class Post {
 
+    static class Offer {
+
+        // public void createOfferPost
+            // this will include information such as driver name and rating,
+            // departure time and date, destination, max # of riders, route
+
+        // public void removeOfferPost
+    }
+
+    static class Request {
+        // public void createRequestPost
+        // public void removeRequestPost
+    }
 }
 
-public class Request {
-    // public void createRequestPost
-    // public void removeRequestPost
-}
