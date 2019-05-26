64f215419ddb3b5b2e12e2b82f45010ab4a84574
Samuel Sachnoff <ssachnof@calpoly.edu>
2019-05-26 14:02:39 -0700

    updated Registration page to push additional info to the database

--- app/src/main/java/com/example/carpooly/DatabaseWriter.java
+++ app/src/main/java/com/example/carpooly/DatabaseWriter.java
@@ -1,7 +1,6 @@
 package com.example.carpooly;
 
 // preforms writes and reads from the database data tree
-public interface accessDatabase {
+public interface DatabaseWriter {
     public void write();
-    public void read();
 }
--- app/src/main/java/com/example/carpooly/Model/RegistrationModel.java
+++ app/src/main/java/com/example/carpooly/Model/RegistrationModel.java
@@ -1,8 +1,6 @@
 package com.example.carpooly.Model;
 
 import android.content.Context;
-import android.os.Environment;
-import android.util.JsonWriter;
 
 /*import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -12,52 +10,48 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;*/
 
+import com.example.carpooly.DatabaseWriter;
+import com.example.carpooly.HashMapInitializer;
 import com.example.carpooly.UserObject;
-import com.example.carpooly.accessDatabase;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.BufferedReader;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.StringWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.security.KeyStore;
-import java.util.LinkedHashMap;
-import java.util.Scanner;
-
-import javax.xml.parsers.FactoryConfigurationError;
-
-public class RegistrationModel{
+public class RegistrationModel implements DatabaseWriter {
     private String email;
     private String pass;
     private String confirm_pass;
+    private String name;
+    private String phoneNumber;
+
     private Context registrationContext;
     private FirebaseAuth auth;
     private FirebaseUser user;
     private UserObject newUser;
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    //todo: make sure that at some point you modify the security settings on the db so that a user
+    // can't write to another one's data!!!!!!
 
-    public RegistrationModel(String email, String pass, String confirm_pass,
-                             Context registrationContext) {
+
+    public RegistrationModel(String email, String pass, String confirm_pass, String phoneNumber,
+                             String firstName, String lastName, Context registrationContext) {
         this.email = email;
         this.pass = pass;
         this.confirm_pass = confirm_pass;
+        this.name = firstName + " " + lastName;
+        this.phoneNumber = phoneNumber;
         this.registrationContext = registrationContext;
         this.auth = FirebaseAuth.getInstance();
+
     }
     public Task<AuthResult> registerUser() {
         return auth.createUserWithEmailAndPassword(email, pass);
@@ -71,6 +65,10 @@ public class RegistrationModel{
         return pass;
     }
 
+    public String getName(){return name;}
+
+    public String getPhoneNumber(){return phoneNumber;}
+
     public void setAuth() {
         this.auth = FirebaseAuth.getInstance();
     }
@@ -79,9 +77,14 @@ public class RegistrationModel{
         this.user = auth.getCurrentUser();
         return user;
     }
-//    @Override
-//    public void write(){}
-//
-//    @Override
-//    public void read(){}
+
+    @Override
+    public void write(){
+        DatabaseReference ref = database.getReference().child("Users").child(user.getUid());
+        HashMapInitializer<String, Serializable> hashInitializer = new HashMapInitializer<>();
+        HashMap<String, Serializable> userData = hashInitializer.
+                makeHash(Arrays.asList("Name", "Rating", "Email", "Phone", "PrivacyModeActive"),
+                        Arrays.<Serializable>asList(this.name, 3.0, this.email, this.phoneNumber, true));
+        ref.setValue(userData);
+    }
 }
