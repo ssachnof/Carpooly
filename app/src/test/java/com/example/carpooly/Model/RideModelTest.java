69667c711562e4f6ff8f40d23e6d4babce585fef
DHalland <dylanhalland@gmail.com>
2019-06-02 13:01:55 -0700

    included test case templates for Model.

---
+++ app/src/test/java/com/example/carpooly/Model/RideModelTest.java
@@ -0,0 +1,12 @@
+package com.example.carpooly.Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RideModelTest {
+
+    @Test
+    public void setDriverDisplayName() {
+    }
+}
