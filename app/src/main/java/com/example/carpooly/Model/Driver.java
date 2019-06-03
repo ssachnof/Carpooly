fbd6161be5ebac388012b36289cb4cc75c00fa4b
Samuel Sachnoff <ssachnof@calpoly.edu>
2019-06-02 17:00:00 -0700

    Merge branch 'feature-home-screen-display-ride' of https://github.com/ssachnof/Carpooly into temp
    # Conflicts:
    #	app/src/main/java/com/example/carpooly/Controller/CreateRideActivity.java
    #	app/src/main/java/com/example/carpooly/Model/UsersSearch.java
    #	app/src/main/java/com/example/carpooly/Search.java

---
+++ app/src/main/java/com/example/carpooly/Model/Driver.java
@@ -0,0 +1,19 @@
+package com.example.carpooly.Model;
+
+import android.content.Context;
+
+public class Driver extends UserInfoModel {
+    private String driverName;
+
+
+    public Driver(Context context){
+        super(context);
+    }
+
+//    public String findDriverName(String UId){
+//
+//    }
+
+
+
+}
---
+++ app/src/main/java/com/example/carpooly/Model/Rider.java
@@ -0,0 +1,12 @@
+package com.example.carpooly.Model;
+
+import android.content.Context;
+
+public class Rider extends UserInfoModel {
+    public Rider(Context context){
+        super(context);
+    }
+
+//    public String getRiderName(String UId){}
+
+}
