58346c9b2800b4572cb76f5e8a6ee87dddb132ae
Samuel Sachnoff <ssachnof@calpoly.edu>
2019-06-05 13:36:17 -0700

    fixed constraint issue

---
+++ app/src/androidTest/java/com/example/RideModelTest.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.carpooly.Model.RideModel;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class RideModelTest {
+
+    @Test
+    public void setDriverDisplayName() {
+    }
+}
